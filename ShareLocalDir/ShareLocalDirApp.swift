4ecb33f754a6ffbdc1855876aaaa93df1832358b
outman <pochonlee@gmail.com>
2026-06-18 15:46:32 +0800

    Initial Commit

---
+++ ShareLocalDir/ShareLocalDirApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLocalDirApp.swift
+//  ShareLocalDir
+//
+//  Created by LI on 2026/6/18.
+//
+
+import SwiftUI
+
+@main
+struct ShareLocalDirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
